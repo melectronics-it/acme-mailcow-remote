7e7f80ac7ef12fce90aa4cc7afc4e112268a3546
M-Electronics <info@mmehn.de>
2022-07-03 17:03:07 +0200

    Add Placeholders to 'acme-mailcow-npm.sh'

--- acme-mailcow-npm.sh
+++ acme-mailcow-npm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-scp /etc/letsencrypt/live/mail.mmehn.de-0001/fullchain.pem root@'10.0.0.5':/opt/mailcow-dockerized/data/assets/ssl/cert.pem
-scp /etc/letsencrypt/live/mail.mmehn.de-0001/privkey.pem root@'10.0.0.5':/opt/mailcow-dockerized/data/assets/ssl/key.pem
-ssh root@'10.0.0.5' -t "
+scp /etc/letsencrypt/live/<mail-zertifikats-ordnername>/fullchain.pem root@'<mailcow-ip>':/opt/mailcow-dockerized/data/assets/ssl/cert.pem
+scp /etc/letsencrypt/live/mail-zertifikats-ordnername/privkey.pem root@'<mailcow-ip>':/opt/mailcow-dockerized/data/assets/ssl/key.pem
+ssh root@'<mailcow-ip>' -t "
 docker restart mailcowdockerized_postfix-mailcow_1 &&
 docker restart mailcowdockerized_nginx-mailcow_1 &&
 docker restart mailcowdockerized_dovecot-mailcow_1
