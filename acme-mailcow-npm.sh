76a490a19ede68fc5116a0b02e9bb6b7620886b8
M-Electronics <info@mmehn.de>
2022-07-03 17:34:51 +0200

    Improvements...

--- acme-mailcow-npm.sh
+++ acme-mailcow-npm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mailcow_server='<your mailcow server ip address>'
 cert_folder_name="<your-cert-directory-name>"
-scp /etc/letsencrypt/live/$cert_folder_name/fullchain.pem root@'$mailcow_server':/opt/mailcow-dockerized/data/assets/ssl/cert.pem
-scp /etc/letsencrypt/live/$cert_folder_name/privkey.pem root@'$mailcow_server':/opt/mailcow-dockerized/data/assets/ssl/key.pem
-ssh root@'$mailcow_server' -t "
+scp /etc/letsencrypt/live/$cert_folder_name/fullchain.pem root@$mailcow_server:/opt/mailcow-dockerized/data/assets/ssl/cert.pem
+scp /etc/letsencrypt/live/$cert_folder_name/privkey.pem root@$mailcow_server:/opt/mailcow-dockerized/data/assets/ssl/key.pem
+ssh root@$mailcow_server -t "
 docker restart mailcowdockerized_postfix-mailcow_1 &&
 docker restart mailcowdockerized_nginx-mailcow_1 &&
 docker restart mailcowdockerized_dovecot-mailcow_1
