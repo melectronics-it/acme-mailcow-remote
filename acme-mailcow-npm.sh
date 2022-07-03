8340712eb9771370b72dbc434a1872aa11e9c723
M-Electronics <info@mmehn.de>
2022-07-03 17:20:55 +0200

    Add Variables for some values

--- acme-mailcow-npm.sh
+++ acme-mailcow-npm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-scp /etc/letsencrypt/live/<mail-zertifikats-ordnername>/fullchain.pem root@'<mailcow-ip>':/opt/mailcow-dockerized/data/assets/ssl/cert.pem
-scp /etc/letsencrypt/live/mail-zertifikats-ordnername/privkey.pem root@'<mailcow-ip>':/opt/mailcow-dockerized/data/assets/ssl/key.pem
-ssh root@'<mailcow-ip>' -t "
+mailcow_server='<your mailcow server ip address>'
+cert_folder_name="<your-cert-directory-name>"
+scp /etc/letsencrypt/live/$cert_folder_name/fullchain.pem root@'$mailcow_server':/opt/mailcow-dockerized/data/assets/ssl/cert.pem
+scp /etc/letsencrypt/live/$cert_folder_name/privkey.pem root@'$mailcow_server':/opt/mailcow-dockerized/data/assets/ssl/key.pem
+ssh root@'$mailcow_server' -t "
 docker restart mailcowdockerized_postfix-mailcow_1 &&
 docker restart mailcowdockerized_nginx-mailcow_1 &&
 docker restart mailcowdockerized_dovecot-mailcow_1
